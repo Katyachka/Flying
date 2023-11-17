7df00b8f15fe9f859bef4cee8d663ab4236c1f82
Oleg <dolotinod20@gmail.com>
2023-11-18 00:00:43 +0200

    fix run_bot

--- bot/apps/botapp/main_bot.py
+++ bot/apps/botapp/main_bot.py
@@ -2,14 +2,14 @@ from telebot.async_telebot import AsyncTeleBot
 
 from django.conf import settings
 
-bot = AsyncTeleBot(settings.BOT_TOCKEN, parse_mode='Markdown')
+bot = AsyncTeleBot(settings.BOT_TOKEN, parse_mode='Markdown')
 
 
 @bot.message_handler(commands = ['start', 'help'])
 async def send_welcome(message):
-    await bot.reply_to(message, 'Вас вітає Flying бот, який допоможе вам придбати авіаквитки✈️')
+    await bot.send_message(message.chat.id, f'{message.chat.first_name}, вітаємо Вас у Flying бот. Цей бот допоможе Вам придбати авіаквитки✈️')
 
 
 @bot.message_handler(func = lambda message: True)
 async def error_message(message):
-    await bot.reply_to(message, f"Вибачте, {message.text} команда не підтримується👉👈")
+    await bot.reply_to(message, f"Вибачте, '*{message.text}*' команда не підтримується👉👈")
